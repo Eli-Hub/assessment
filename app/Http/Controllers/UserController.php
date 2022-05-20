09abe148b2da69733d83c2cad20bb6ad8df7f374
quoeku <quoeku>
2022-05-20 19:17:49 +0000

    working on filter challenge

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,9 +7,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function fectchUsers(){
+    public function fetchUsers(){
 
         return User::all();
+    }
+
+    public function filterUsers(){
+        $name = readline("Username: ");
+        $role = readline("User Role: ");
+        $email = readline("Email: ");
+        User::where('name', '=', $name)->orwhere('role', '=', $role)->orwhere('email', '=', $email);
+
+        return $this->filterUsers();
     }
 
 
